1510fc007a9851375b4cbb518e9db5732c9b41f8
Ahmed Albishri <ahmad.al.bishri@gmail.com>
2017-04-12 01:51:19 -0500

    update on code

--- label_image.py
+++ label_image.py
@@ -2,24 +2,52 @@ import tensorflow as tf, sys
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS, cross_origin
 import base64
+import tensorflow as tf, sys
+import urllib, cStringIO
+import os
+import wget
 
-# webapp
 app = Flask(__name__)
-#image_path = sys.argv[1]
 
+# def download_image(img_str, url):
+#     # with open(img_str, "wb") as fh:
+#     #     fh.write(urllib.urlopen(url).read())
+#     f = open(img_str, 'wb')
+#     dat = urllib.urlopen(url).read()
+#     print "dat",dat
+#     f.write(str(dat))
+#     print("written ", img_str, " ", url)
+#     f.close()
 
-@app.route('/api/predict', methods=['POST'])
+def remove_images():
+    import os
+    path = os.path.abspath('static/tmp')
+    for file_name in os.listdir('static/tmp'):
+        os.remove(path+file_name)
+
+@app.route('/', methods=['POST'])
 @cross_origin()
-def predict():
-    data = request.values['imageBase64']
-    with open("imageToPredict.jpeg", "wb") as fh:
-        fh.write(base64.decodestring(data))
-    # image_data = re.sub('^data:image/.+;base64,', '', image_b64).decode('base64')
+def method():
+    print "request received"
+    data = request.get_json()
+    print "data",data
+    print "json_data",data['imageBase64']
+    image_list_str=data['imageBase64']
+    print "image_list",image_list_str
 
-    image_path = 'imageToPredict.jpeg'
-    # Read in the image_data
-    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+    file_name = []
+    for counter, image_url in enumerate(image_list_str):
+        import os
+        print image_url
+        path=os.path.abspath('./static/tmp/')
+        print path
 
+        file_name.append(wget.download(image_url))
+        print(file_name)
+
+
+    print("loop complete")
+    tf_predictions = []
     # Loads label file, strips off carriage return
     label_lines = [line.rstrip() for line
                    in tf.gfile.GFile("data/output_labels.txt")]
@@ -30,26 +58,23 @@ def predict():
         graph_def.ParseFromString(f.read())
         _ = tf.import_graph_def(graph_def, name='')
 
-    with tf.Session() as sess:
-        # Feed the image_data as input to the graph and get first prediction
-        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    for file in file_name:
+        # Read in the image_data
+        print file
+        image_data = tf.gfile.FastGFile(file, 'rb').read()
 
-        predictions = sess.run(softmax_tensor, \
+        with tf.Session() as sess:
+            # Feed the image_data as input to the graph and get first prediction
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+            predictions = sess.run(softmax_tensor, \
                                {'DecodeJpeg/contents:0': image_data})
 
-        # Sort to show labels of first prediction in order of confidence
-        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
-        output_string = []
-        output_score = []
-        for node_id in top_k:
-            human_string = label_lines[node_id]
-            score = predictions[0][node_id]
-            output_string.append(human_string)
-            output_score.append(score)
-            print('%s (score = %.5f)' % (human_string, score))
-    return jsonify(results=[output_string])
+            # Sort to show labels of first prediction in order of confidence
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            tf_predictions.append(label_lines[top_k[0]])
 
+    print tf_predictions
 
-@app.route('/')
-def main():
-    return render_template('index.html')
+    # remove_images()
+    return repr(tf_predictions)
